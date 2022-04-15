63cb865f3167f18a8d6949e22673de25c056b81e
eswar <neelaveni.eswar04@gmail.com>
2022-04-15 13:21:40 +0530

    [Eswar] Added Delete program in addressbook

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -11,14 +11,26 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
-           // Person.AddPerson();
-            //Person.ListPeople();
-            //Person1.AddPerson();
-            //Person1.ListPeople();
-            Person2.AddPerson();
-            Person2.Modify();
-            Person2.ListPeople();
-            Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("select numbers \n1)Add Contact\n2)Modify Person\n3)listPeople\n4)delete person");
+                int Option = Convert.ToInt32(Console.ReadLine());
+                switch (Option)
+                {
+                    case 1:
+                        Person2.AddPerson();
+                        break;
+                    case 2:
+                        Person2.Modify();
+                        break;
+                    case 3:
+                        Person2.ListPeople();
+                        break;
+                    case 4:
+                        Person3.Delete();
+                        break;
+                }
+            }
         }
     }
 }
